d30960531ea3e4f2b23769757075abb080311e98
Leonard Nowak <146101967+Leonowak@users.noreply.github.com>
2025-03-26 15:43:26 +0100

    Update base-eval.py

--- model/baseline/base-eval.py
+++ model/baseline/base-eval.py
@@ -45,3 +45,4 @@ if __name__ == "__main__":
 #de-nl-pred = TER 78.82; METEOR: 0.29; BLEU: 13.87503500272944
 #de-en-pred = TER 109.11; METEOR: 0.02; BLEU: 0.8775030611323054
 #en-nl-pred = TER 104.86; METEOR: 0.03; BLEU: 0.876038895010554
+# add comet for sure!
